0b49596b275728d1d1287c17333d1d4134ba755c
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2026-03-02 18:38:51 +0300

    [DOP-31721] Validate SQL transformation FROM clause

--- tests/test_unit/test_transfers/test_create_transfer.py
+++ tests/test_unit/test_transfers/test_create_transfer.py
@@ -1422,6 +1422,8 @@ async def test_developer_plus_can_create_transfer_with_sql_transformation(
     "query",
     [
         "SELECT col1, col2 FROM source WHERE col1 > 100;",
+        "SELECT col1, col2\nFROM source\nWHERE col1 > 100;",
+        "SELECT col1, col2\r\nFROM source\r\nWHERE col1 > 100;",
         "select COL1 from SOURCE",
         "   SELECT   col1   FROM   source  ",
         " WITH some AS (SELECT col1 FROM source) SELECT * FROM some;",
